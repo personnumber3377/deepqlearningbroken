ff16b0d671715ad4f151695edfa33965bfc8cf23
personnumber3377 <63960351+personnumber3377@users.noreply.github.com>
2021-02-13 19:04:10 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,12 +6,11 @@ import random
 import time as time1
 
 def relu(mat):
-    #print(np.multiply(mat,(mat>0)))
-    #print(mat)
+    
     return mat*(mat>0)
     
 def relu_derivative(mat):
-    #print((mat>0)*1)
+    
     return (mat>0)*1
 
 
@@ -44,18 +43,18 @@ class Neuron:
 
     def sigmoid(self, x):
         x = round(x, 3)
-        #print(x)
+        
         if x > 500:
-        	#print("fefewfewfewfewgew")
+        	
         	return 1
         if x < -500:
         	return 0
         return (1/(1+(math.exp(-1*x))))
-        #return relu(x)
+        
 
     def dSigmoid(self, x):
         return self.sigmoid(x)*(1-self.sigmoid(x))
-        #return relu_derivative(x)
+        
     def setError(self, err):
         self.error = err
 
@@ -77,10 +76,7 @@ class Neuron:
         self.gradient = self.error * self.dSigmoid(self.output);
         
         for dendron in self.dendrons:
-            #print(self.gradient)
-            #print(str(dendron.connectedNeuron.output) + " !!")
             
-            #print("huuuttia")
             dendron.dWeight = Neuron.eta * (self.gradient * dendron.connectedNeuron.output) + self.alpha * dendron.dWeight;
             dendron.weight = dendron.weight - dendron.dWeight;  #was originally adding, but changed it into subtracting
             dendron.connectedNeuron.addError(dendron.weight * self.gradient);
@@ -112,13 +108,13 @@ class Network:
 
     def backPropagate(self, loss):
         for i in range(0, len(self.layers[-1])-1):
-            #print(str(len(self.layers[-1])) + " !!!!!!")
+            
             #self.layers[-1][i].setError(self.layers[-1][i].getOutput()- target[i]) was originally this
             self.layers[-1][i].setError(loss[i])
         for layer in self.layers[::-1]:
             for neuron in layer:
                 neuron.backPropagate()
-        #print("ooff")
+        
 
     def getError(self, target):
         err = 0
@@ -182,10 +178,7 @@ for howmanytimes in range(0, howmanytimesrange):
     while True:
         oldobservation = observation
         time += 1
-        #if time > 50:
-        #    good = True
-        #    print("OOOFFFFF")
-        #    break
+        
         policynetwork.setInput(observation)
         policynetwork.feedForword()
         explorationorexploitation = np.random.uniform()
@@ -199,27 +192,19 @@ for howmanytimes in range(0, howmanytimesrange):
         policynetwork.setInput(observation)
         policynetwork.feedForword()
 
-        #print(observation)
-        #print(policynetwork.getResults())
-        #print(action)
+        
         
         
         
 
         observation, reward, done, info = environment.step(action)
 
-        #print(str(observation.shape)+" !!!!!!!!!!!")
-        #if howmanytimes > 200:
-        #    environment.render()
-        #environment.render()
-        #print(done)
+        
         if done:
             reward = -1
 
         experiences.append([oldobservation, action, reward, observation, done])
-        #print([oldobservation, action, reward, observation])
-        #print([oldobservation, action, reward, observation])
-        #print(experiences[0])
+        
         totalloss = 0
         
         totalloss = 0
@@ -230,13 +215,13 @@ for howmanytimes in range(0, howmanytimesrange):
 
         for i in range(samplespertrainingtime):
 
-            #sampletobotrained = random.choice(experiences)
+            
             try:
                 sampletobotrained = experiences[random.randint(0, len(experiences)-1)]
             except:
                 sampletobotrained = experiences[0]
                 print("ooooooffffff")
-            #print(sampletobotrained)
+            
             if len(experiences) == 0:
                 print("huutista")
             
@@ -257,70 +242,42 @@ for howmanytimes in range(0, howmanytimesrange):
             qprimesa = targetnetwork.getResults()
             if sampletobotrained[4]:
             	qprimesa[sampletobotrained[1]] = -1
-            	#print("get nea nead")
+            	
             else:
             	qprimesa[sampletobotrained[1]] = (float(sampletobotrained[2])+(float(gamma)*float(max(qprimesa))))
-            	#print("dont get nea nead")
+            	
             loss = []
             
-            """
-            print("Reward: " + str(sampletobotrained[2]))
-            print("Gamma: " + str(gamma))
-            print("Maximum of q prime: " + str(max(qprimesa)))
-            print("Q of s and a: " + str(qsa[sampletobotrained[1]]))
-
-            print("Result loss: " + str(((float(sampletobotrained[2])+(float(gamma)*float(max(qprimesa))))-(qsa[sampletobotrained[1]]))**2))
-            """
-            """
-            for paska in range(0, len(qprimesa)):
-                loss.append(((float(sampletobotrained[2])+(float(gamma)*float(max(qprimesa))))-(qsa[paska])))
-                #print(((float(sampletobotrained[2])+(float(gamma)*float(max(qprimesa))))-(qsa[sampletobotrained[1]]))**2)
-                #print(((float(sampletobotrained[2])+(float(gamma)*float(max(qprimesa))))-(qsa[sampletobotrained[1]]))**2)
-                totalloss += ((sampletobotrained[2]+gamma*max(qprimesa))-(qsa[paska]))
-
-
-               
-                if sampletobotrained[1] == 1:
-                    totalloss1 +=((sampletobotrained[2]+(gamma*max(qprimesa)))-(qsa[paska]))
-                    totalones += 1
-                if sampletobotrained[1] == 0:
-                    totalloss0 +=((sampletobotrained[2]+(gamma*max(qprimesa)))-(qsa[paska]))
-                    totalzeros += 1
-            """
+            
             loss = [0,0]
             for i in range(0, len(qsa)):
                 loss[i] = qsa[i]-qprimesa[i]
-            #print(loss)
-                
-            #originally was this whole thing squared on both of these
-            #print(loss)
+            
 
 
             
             policynetwork.backPropagate(loss)
 
-            #time.sleep(0.3)
-            #print(loss)
+            
 
 
-            #print("Average total loss:" +str(totalloss/samplespertrainingtime))
+            
         if len(experiences) > memorysize:
 
             experiences.pop(0)
-        #print(len(experiences))
+        
 
             
             
     
 
         
-        #environment.render()
+        
         if done:
             environment.reset()
             time = 0
             break
-    #if good:
-    #    break 
+    
     
     
     
@@ -348,9 +305,3 @@ while True:
         environment.reset()
 
 
-"""
-
-note to self:
-the reason why the code outputs very low or very high values, is because it takes the argmax of the neural network output, so even when there is a very slight difference between the output it doesn't matter,
-because it still takes the one with the most value, so it does not matter how close they are or it could be the way the loss is calculated i dunno
-"""
